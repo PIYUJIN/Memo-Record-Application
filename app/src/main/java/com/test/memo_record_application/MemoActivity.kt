a122a816a724fa74858bc45c8fcb80864faccd3a
PIYUJIN <yjyj020201@gmail.com>
2023-07-06 15:21:24 +0900

    design memo activity

--- app/src/main/java/com/test/memo_record_application/MemoActivity.kt
+++ app/src/main/java/com/test/memo_record_application/MemoActivity.kt
@@ -1,11 +1,37 @@
 package com.test.memo_record_application
 
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.test.memo_record_application.databinding.ActivityMemoBinding
 
 class MemoActivity : AppCompatActivity() {
+
+    lateinit var activityMemoBinding: ActivityMemoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_memo)
+        activityMemoBinding = ActivityMemoBinding.inflate(layoutInflater)
+        setContentView(activityMemoBinding.root)
+
+        activityMemoBinding.run {
+            toolbarMemo.run {
+                // back button 세팅
+                setNavigationIcon(androidx.appcompat.R.drawable.abc_ic_ab_back_material)
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+                    navigationIcon?.colorFilter = BlendModeColorFilter(Color.WHITE, BlendMode.SRC_ATOP)
+                } else {
+                    navigationIcon?.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP)
+                }
+
+                setNavigationOnClickListener {
+                    finish()
+                }
+            }
+        }
     }
 }
