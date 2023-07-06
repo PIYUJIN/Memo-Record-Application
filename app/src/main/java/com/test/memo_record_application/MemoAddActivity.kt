c545841c5514dc52f27a57e6f06416c3beafa860
PIYUJIN <yjyj020201@gmail.com>
2023-07-06 15:34:49 +0900

    design memo add activity

--- app/src/main/java/com/test/memo_record_application/MemoAddActivity.kt
+++ app/src/main/java/com/test/memo_record_application/MemoAddActivity.kt
@@ -1,11 +1,41 @@
 package com.test.memo_record_application
 
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.test.memo_record_application.databinding.ActivityMemoAddBinding
 
 class MemoAddActivity : AppCompatActivity() {
+
+    lateinit var activityMemoAddBinding: ActivityMemoAddBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_memo_add)
+        activityMemoAddBinding = ActivityMemoAddBinding.inflate(layoutInflater)
+        setContentView(activityMemoAddBinding.root)
+
+        activityMemoAddBinding.run {
+            toolbarMemoAdd.run {
+                title = "메모 추가"
+                setTitleTextColor(Color.WHITE)
+                inflateMenu(R.menu.add_menu)
+
+                // back button 세팅
+                setNavigationIcon(androidx.appcompat.R.drawable.abc_ic_ab_back_material)
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+                    navigationIcon?.colorFilter = BlendModeColorFilter(Color.WHITE, BlendMode.SRC_ATOP)
+                } else {
+                    navigationIcon?.setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP)
+                }
+
+                setNavigationOnClickListener {
+                    finish()
+                }
+            }
+        }
     }
 }
