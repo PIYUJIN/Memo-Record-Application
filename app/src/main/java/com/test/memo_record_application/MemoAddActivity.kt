5ebcec974c546328518763485814abd6b54ee3dd
PIYUJIN <yjyj020201@gmail.com>
2023-07-06 15:22:26 +0900

    create memo add activity

---
+++ app/src/main/java/com/test/memo_record_application/MemoAddActivity.kt
@@ -0,0 +1,11 @@
+package com.test.memo_record_application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MemoAddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memo_add)
+    }
+}
